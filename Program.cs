e879e883d7040a6554d9771da445950d9f642bfa
Alex <garridoalex77@gmail.com>
2016-08-01 09:42:15 -0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmptyConsole1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 6;
+            a %= b;
+            Console.WriteLine(a);
+        }
+    }
+}

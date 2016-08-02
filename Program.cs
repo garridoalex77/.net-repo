92e20eb028d0a9ceb69f75ff8ec5049d724909ec
Alex <garridoalex77@gmail.com>
2016-08-02 08:12:39 -0500

    Narcissistic numbers in c#

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EmptyConsole1
 {
@@ -9,18 +7,32 @@ namespace EmptyConsole1
     {
         public static void Main(string[] args)
         {
-            int x = 42;
-            switch (x)
+            List<int> allNarns = new List<int>();
+            int num = 0;
+            int numArrLength = 0;
+            int i;
+            do
             {
-                case 42:
-                case 33:
-                    Console.WriteLine("Its 42");
-                    Console.WriteLine("is it 33");
-                    break;
-                default:
-                    Console.WriteLine("not valid");
-                    break;
-            }
+                double check = 0;
+                int numLength = num.ToString().Length;
+                i = 0;
+                do
+                {
+                    char[] numArr = num.ToString().ToCharArray();
+                    numArrLength = numArr.Length;
+                    check += Math.Pow(Int32.Parse(numArr[i].ToString()), numLength);
+                    i++;
+                    
+                    if (check > num) {
+                        break;
+                    }
+                } while (i < numLength);
+                if (num == check) {
+                    allNarns.Add(num);
+                    Console.WriteLine(num);
+                }
+                num++;
+            } while (allNarns.ToArray().Length < 25);
         }
     }
 }
